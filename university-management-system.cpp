5de445575e48585203440096e8024f4dc8292065
Simran Verma <88669720+Simranverma123@users.noreply.github.com>
2021-10-22 02:00:19 +0530

    Update university-management-system.cpp
    Added student structure and related function

--- university-management-system.cpp
+++ university-management-system.cpp
@@ -1,2 +1,67 @@
 #include<iostream>
 using namespace std;
+struct Student
+{
+    int roll;
+    string name;
+    string branch_id;
+    int cgpa;
+    struct Student *next;
+    struct Student *prev;
+};
+void create(struct Student **start,struct Student **last)
+{
+  Student *nw=new Student;
+ cout<<"\nEnter Name:-";
+ cin>>nw->name;
+ cout<<"\nEnter Rollno:";
+ cin>>nw->roll;
+ cout<<"\nEnter Branch ID:-";
+ cin>>nw->branch_id;
+ cout<<"\nEnter CGPA:-";
+ cin>>nw->cgpa;
+ nw->next=NULL;
+ nw->prev=NULL;
+ 
+ if(*start==NULL)
+ {
+     *start=nw;
+     *last=nw;  
+ }
+ else
+ {
+     (*last)->next=nw;
+     nw->prev=*last;
+     *last=nw;
+
+ }
+}
+void display(struct Student **start,struct Student **last )
+{
+    struct Student *t;
+    t=*start;
+    while(t!=NULL)
+    {
+        cout<<"Roll no: "<<t->roll<<endl;
+        cout<<"NAME: "<<t->name<<endl;
+        cout<<"Branch ID: "<<t->branch_id<<endl;
+        cout<<"CGPA: "<<t->cgpa<<endl;
+       t=t->next;
+       cout<<endl;
+ 
+    }
+}
+int main()
+{
+    struct Student *st=NULL;
+    struct Student *l=NULL;
+    char ch='y';
+    while(ch=='y')
+    {
+        create(&st,&l);
+        cout<<"Add more(y/n)?"<<endl;
+        cin>>ch;
+    }
+    display(&st,&l);
+    return 0;
+}
